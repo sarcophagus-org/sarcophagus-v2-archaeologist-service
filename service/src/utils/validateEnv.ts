8031717695976cd1fe2be884c7026e9844541bf5
Kellar <kellar.base09@gmail.com>
2022-08-11 16:37:05 +0100

    minor update

--- service/src/utils/validateEnv.ts
+++ service/src/utils/validateEnv.ts
@@ -4,7 +4,7 @@ import { BigNumber, constants } from "ethers"
 
 import ethers from "ethers";
 
-const tryReadEnv = (envName: string, envVar: string | undefined, operation?: (envVar: string) => any) => {
+const _tryReadEnv = (envName: string, envVar: string | undefined, operation?: (envVar: string) => any) => {
     if (!envVar) {
         throw Error(`${envName} not set in .env`)
     }
@@ -19,13 +19,13 @@ const tryReadEnv = (envName: string, envVar: string | undefined, operation?: (en
 }
 
 export function validateEnvVars(): PublicEnvConfig {
-    tryReadEnv("IP_ADDRESS", process.env.IP_ADDRESS);
-    tryReadEnv("TCP_PORT", process.env.TCP_PORT);
-    tryReadEnv("WS_PORT", process.env.WS_PORT);
-    tryReadEnv("SIGNAL_SERVER_LIST", process.env.SIGNAL_SERVER_LIST);
+    _tryReadEnv("IP_ADDRESS", process.env.IP_ADDRESS);
+    _tryReadEnv("TCP_PORT", process.env.TCP_PORT);
+    _tryReadEnv("WS_PORT", process.env.WS_PORT);
+    _tryReadEnv("SIGNAL_SERVER_LIST", process.env.SIGNAL_SERVER_LIST);
 
     if (process.env.IS_BOOTSTRAP !== "true") {
-        tryReadEnv("BOOTSTRAP_LIST", process.env.BOOTSTRAP_LIST);
+        _tryReadEnv("BOOTSTRAP_LIST", process.env.BOOTSTRAP_LIST);
     }
 
     const publicConfig = validateBlockEnvVars();
@@ -42,17 +42,17 @@ function validateBlockEnvVars() {
         feePerByte: constants.Zero,
     };
 
-    tryReadEnv("CHAIN_ID", process.env.CHAIN_ID, (envVar) => {
+    _tryReadEnv("CHAIN_ID", process.env.CHAIN_ID, (envVar) => {
         if (Number.isNaN(Number.parseInt(envVar))) throw "";
     });
-    tryReadEnv("PROVIDER_URL", process.env.PROVIDER_URL);
-    tryReadEnv("SARCO_DIAMOND_ADDRESS", process.env.SARCO_DIAMOND_ADDRESS);
-    tryReadEnv("SARCO_TOKEN_ADDRESS", process.env.SARCO_TOKEN_ADDRESS);
+    _tryReadEnv("PROVIDER_URL", process.env.PROVIDER_URL);
+    _tryReadEnv("SARCO_DIAMOND_ADDRESS", process.env.SARCO_DIAMOND_ADDRESS);
+    _tryReadEnv("SARCO_TOKEN_ADDRESS", process.env.SARCO_TOKEN_ADDRESS);
 
 
-    tryReadEnv("ETH_PRIVATE_KEY", process.env.ETH_PRIVATE_KEY);
+    _tryReadEnv("ETH_PRIVATE_KEY", process.env.ETH_PRIVATE_KEY);
 
-    tryReadEnv(
+    _tryReadEnv(
         "ENCRYPTION_PRIVATE_KEY",
         process.env.ENCRYPTION_PRIVATE_KEY,
         (envVar) => {
@@ -61,7 +61,7 @@ function validateBlockEnvVars() {
         }
     );
 
-    tryReadEnv(
+    _tryReadEnv(
         "MAX_RESURRECTION_TIME",
         process.env.MAX_RESURRECTION_TIME,
         (envVar) => {
@@ -69,22 +69,22 @@ function validateBlockEnvVars() {
             publicConfig.maxResurrectionTime = Number.parseInt(envVar)
         }
     );
-    tryReadEnv(
+    _tryReadEnv(
         "MIN_BOUNTY",
         process.env.MIN_BOUNTY,
         (envVar) => publicConfig.minBounty = BigNumber.from(envVar)
     );
-    tryReadEnv(
+    _tryReadEnv(
         "MIN_DIGGING_FEES",
         process.env.MIN_DIGGING_FEES,
         (envVar) => publicConfig.minDiggingFees = BigNumber.from(envVar)
     );
-    tryReadEnv(
+    _tryReadEnv(
         "FEE_PER_BYTE",
         process.env.FEE_PER_BYTE,
         (envVar) => publicConfig.feePerByte = BigNumber.from(envVar)
     );
-    tryReadEnv(
+    _tryReadEnv(
         "IS_ARWEAVER",
         process.env.IS_ARWEAVER,
         (envVar) => publicConfig.isArweaver = envVar === "true"
