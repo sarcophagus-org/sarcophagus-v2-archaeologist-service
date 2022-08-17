2948bbf250a4564f511076c6bb23560ff088ea9e
Kellar <kellar.base09@gmail.com>
2022-08-17 16:06:36 +0100

    cleanup

--- service/src/utils/health-check.ts
+++ service/src/utils/health-check.ts
@@ -1,4 +1,4 @@
-import { BigNumber, constants, ethers } from "ethers";
+import { constants, ethers } from "ethers";
 import { exit } from "process";
 import { Web3Interface } from "scripts/web3-interface";
 import { RPC_EXCEPTION } from "./exit-codes";
@@ -16,41 +16,18 @@ export async function healthCheck(web3Interface: Web3Interface) {
 
         console.log(` * SARCO:             ${ethers.utils.formatEther(bal)} SARCO`,);
         console.log(` * ETHER:             ${ethers.utils.formatEther(ethBal)} ETH`,);
-        // if (ethBal.eq(constants.Zero)) {
-        console.log(`\n   You will not be able to sign any transactions (or do unwrappings)!\n`);
-        // }
+        if (ethBal.eq(constants.Zero)) {
+            console.log(`\n   You will not be able to sign any transactions (or do unwrappings)!\n`);
+        }
 
         console.log(` * Free Bond:         ${ethers.utils.formatEther(freeBond)} SARCO`,);
-        // if (freeBond.eq(constants.Zero)) {
-        console.log(`\n   You will not be able to accept new jobs!`);
-        console.log(`   Run: \`npm run start -- --deposit-bond:<amount>\` to deposit some SARCO\n`);
-        // }
+        if (freeBond.eq(constants.Zero)) {
+            console.log(`\n   You will not be able to accept new jobs!`);
+            console.log(`   Run: \`npm run start -- --deposit-bond:<amount>\` to deposit some SARCO\n`);
+        }
         console.log("=========================================================================================================\n\n");
     } catch (e) {
-        if (e.reason) {
-            handleRpcError(e.reason);
-        }
-        else {
-            console.error(e);
-        }
+        console.error(e);
         exit(RPC_EXCEPTION);
-    }
-}
-
-function handleRpcError(error: string) {
-    if (error.includes("NotEnoughFreeBond")) {
-        const a = error.indexOf("(") + 1;
-        const b = error.indexOf(",");
-
-        const available = error.substring(a, b);
-        console.error(`\nNot enough free bond. Available: ${ethers.utils.formatEther(available)} SARCO`,)
-    } else if (error.includes("NotEnoughReward")) {
-        const a = error.indexOf("(") + 1;
-        const b = error.indexOf(",");
-
-        const available = error.substring(a, b);
-        console.error(`\nNot enough reward. Available: ${ethers.utils.formatEther(available)} SARCO`,)
-    } else {
-        console.error(error);
     }
 }
