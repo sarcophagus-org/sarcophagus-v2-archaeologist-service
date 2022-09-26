b92b4d47d88a7b6ac8e2dd314654b3f48e1edc1c
Kellar <kellar.base09@gmail.com>
2022-09-26 14:04:24 +0100

    Readability update

--- service/src/test/node_start.ts
+++ service/src/test/node_start.ts
@@ -1,5 +1,3 @@
-import path from 'path';
-import { fileURLToPath } from 'url';
 import { archLogger } from '../utils/chalk-theme';
 import { setupTestSuite } from './test_suite';
 import 'dotenv/config';
@@ -16,12 +14,30 @@ export async function runTests() {
     const testSuite = spawnTestSuite();
     const cleanup = await testSuite.startLocalNetwork();
 
+
+    /**
+    *
+    * TEST: "LISTENING TO MESSAGE STREAM" OUTPUT
+    *
+    **/
     archLogger.warn("\n\n Listens for messages");
     await testSuite.expectOutput('listening to stream on protocol: /message', { ...opts, restartNetwork: false });
 
+
+    /**
+    *
+    * TEST: "BALANCES" OUTPUT
+    *
+    **/
     archLogger.warn("\n\n Shows balances");
     await testSuite.expectOutput('YOUR BALANCES:', opts);
 
+
+    /**
+    *
+    * SETUP FOR "LOW FUNDS" WARNINGS
+    *
+    **/
     const web3Interface = await getWeb3Interface();
 
     // Remove ETH from account
@@ -31,9 +47,23 @@ export async function runTests() {
         value: bal.sub(ethers.utils.parseEther('0.0005'))
     });
 
+
+
+    /**
+    *
+    * TEST: "TOO LITTLE ETH" WARNING
+    *
+    **/
     archLogger.warn("\n\n Shows notice if there's no ETH in account");
     const noEthNotice = 'You have very little ETH in your account. You may not be able to sign any transactions (or do unwrappings)!';
     await testSuite.expectOutput(noEthNotice, opts);
+
+
+    /**
+     *
+     * TEST: NO WARNING IF ENOUGH ETH
+     *
+     **/
 
     // Send ETH back to account
     const bal2 = await web3Interface.encryptionWallet.getBalance();
@@ -41,9 +71,19 @@ export async function runTests() {
         to: web3Interface.ethWallet.address,
         value: bal2.sub(ethers.utils.parseEther('0.0005'))
     });
+
     archLogger.warn("\n\n No notice needed if ETH available");
     await testSuite.expectOutput(noEthNotice, { ...opts, toNotShow: true });
 
+
+
+
+
+    /**
+    *
+    * SETUP FOR FREE BOND TESTS - REGISTER ARCH WITH FREE BOND DEPOSIT
+    *
+    **/
     archLogger.warn("\nRegistering archaeologist");
     const deployerWallet = new Wallet(process.env.TEST_DEPLOYER_PRIVATE_KEY!, web3Interface.ethWallet.provider);
     await web3Interface.sarcoToken.connect(deployerWallet).transfer(web3Interface.ethWallet.address, ethers.utils.parseEther('1000'));
@@ -53,13 +93,26 @@ export async function runTests() {
 
     const noFreeBondNotice = 'You have no free bond. You will not be able to accept new jobs!';
 
+
+    /**
+    *
+    * TEST: NO WARNING IF FREE BOND IN ACCOUNT
+    *
+    **/
     archLogger.warn("\n\n No notice needed if free bond available");
     await testSuite.expectOutput(noFreeBondNotice, { ...opts, toNotShow: true });
+
+
+
+    /**
+     *
+     * TEST: "NO FREE BOND" WARNING
+     *
+     **/
 
     // Withdraw all free bond
     const freeBond = await web3Interface.viewStateFacet.getFreeBond(web3Interface.ethWallet.address);
     await web3Interface.archaeologistFacet.withdrawFreeBond(freeBond);
-
     archLogger.warn("\n\n Shows notice if there's no free bond");
     await testSuite.expectOutput(noFreeBondNotice, opts);
 
