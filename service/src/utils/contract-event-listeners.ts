041a25ed799b5d7a6d80426b18e35f655a222cd3
Kellar <kellar.base09@gmail.com>
2022-09-27 13:57:51 +0100

    some refactors

--- service/src/utils/contract-event-listeners.ts
+++ service/src/utils/contract-event-listeners.ts
@@ -5,46 +5,69 @@ import { RPC_EXCEPTION } from "./exit-codes";
 import { inMemoryStore } from "./onchain-data";
 import { rescheduleUnwrap, scheduleUnwrap } from "./scheduler";
 
+function getCreateSarcoHandler(web3Interface: Web3Interface, archAddress: string) {
+    return (
+        sarcoId,
+        sarcoName,
+        transferrable,
+        resurrectionTime,
+        embalmer,
+        recipient,
+        cursedArchs,
+        totalFees,
+        arweaveTxIds
+    ) => {
+        const isCursed = (cursedArchs as string[]).includes(archAddress);
+        if (isCursed) {
+            const resurrectionDate = new Date(resurrectionTime as number);
+            scheduleUnwrap(web3Interface, sarcoId, resurrectionDate);
+        }
+    }
+}
+
+function getRewrapHandler() {
+    return (sarcoId: string, _) => {
+        archLogger.info(`Sarcophagus cleaned: ${sarcoId}`);
+        inMemoryStore.sarcophagi = inMemoryStore.sarcophagi.filter(s => s.id !== sarcoId);
+    }
+}
+
+function getCleanHandler() {
+    return (sarcoId: string, _) => {
+        archLogger.info(`Sarcophagus cleaned: ${sarcoId}`);
+        inMemoryStore.sarcophagi = inMemoryStore.sarcophagi.filter(s => s.id !== sarcoId);
+    }
+}
+
+function getBuryHandler() {
+    return (sarcoId: string, _) => {
+        archLogger.info(`Sarcophagus buried: ${sarcoId}`);
+        inMemoryStore.sarcophagi = inMemoryStore.sarcophagi.filter(s => s.id !== sarcoId);
+    }
+}
+
 export async function setupEventListeners(web3Interface: Web3Interface) {
     try {
         const archAddress = web3Interface.ethWallet.address;
 
-        const createSarcoFilter = web3Interface.embalmerFacet.filters.CreateSarcophagus();
-        web3Interface.embalmerFacet.on(createSarcoFilter, (
-            sarcoId,
-            sarcoName,
-            transferrable,
-            resurrectionTime,
-            embalmer,
-            recipient,
-            cursedArchs,
-            totalFees,
-            arweaveTxIds
-        ) => {
-            const isCursed = (cursedArchs as string[]).includes(archAddress);
-            if (isCursed) {
-                const resurrectionDate = new Date(resurrectionTime as number);
-                scheduleUnwrap(web3Interface, sarcoId, resurrectionDate);
-            }
-        });
+        const filters = {
+            createSarco: web3Interface.embalmerFacet.filters.CreateSarcophagus(),
+            rewrap: web3Interface.embalmerFacet.filters.RewrapSarcophagus(),
+            clean: web3Interface.thirdPartyFacet.filters.CleanUpSarcophagus(),
+            bury: web3Interface.embalmerFacet.filters.BurySarcophagus(),
+        };
 
-        const rewrapFilter = web3Interface.embalmerFacet.filters.RewrapSarcophagus();
-        web3Interface.embalmerFacet.on(rewrapFilter, (sarcoId, newResurrectionTime) => {
-            const newResurrectionDate = new Date(newResurrectionTime as number);
-            rescheduleUnwrap(web3Interface, sarcoId, newResurrectionDate);
-        });
+        const handlers = {
+            createSarco: getCreateSarcoHandler(web3Interface, archAddress),
+            rewrap: getRewrapHandler(),
+            clean: getCleanHandler(),
+            bury: getBuryHandler(),
+        };
 
-        const cleanFilter = web3Interface.thirdPartyFacet.filters.CleanUpSarcophagus();
-        web3Interface.embalmerFacet.on(cleanFilter, (sarcoId: string, _) => {
-            archLogger.info(`Sarcophagus cleaned: ${sarcoId}`);
-            inMemoryStore.sarcophagi = inMemoryStore.sarcophagi.filter(s => s.id !== sarcoId);
-        });
-
-        const buryFilter = web3Interface.embalmerFacet.filters.BurySarcophagus();
-        web3Interface.embalmerFacet.on(buryFilter, (sarcoId: string, _) => {
-            archLogger.info(`Sarcophagus buried: ${sarcoId}`);
-            inMemoryStore.sarcophagi = inMemoryStore.sarcophagi.filter(s => s.id !== sarcoId);
-        });
+        web3Interface.embalmerFacet.on(filters.createSarco, handlers.createSarco);
+        web3Interface.embalmerFacet.on(filters.rewrap, handlers.rewrap);
+        web3Interface.embalmerFacet.on(filters.clean, handlers.clean);
+        web3Interface.embalmerFacet.on(filters.bury, handlers.bury);
     } catch (e) {
         console.error(e);
         exit(RPC_EXCEPTION);
