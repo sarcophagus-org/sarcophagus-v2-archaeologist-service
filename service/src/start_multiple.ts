587a11a71bf52918ffada912e6b81b2ff091c5b2
Kellar <kellar.base09@gmail.com>
2022-09-30 15:33:58 +0100

    Remove bootstrap setup from start_multiple

--- service/src/start_multiple.ts
+++ service/src/start_multiple.ts
@@ -1,10 +1,6 @@
 import 'dotenv/config'
-import { getMultiAddresses, loadPeerIdFromFile } from "./utils";
-import { randomArchVals } from "./utils/random-arch-gen.js";
-import { Archaeologist } from "./models/archaeologist";
+import { randomTestArchVals } from "./utils/random-arch-gen.js";
 import { Libp2p } from "libp2p";
-import { ethers } from 'ethers';
-import { getWeb3Interface } from './scripts/web3-interface';
 import { startService } from './start_service';
 
 
@@ -13,44 +9,15 @@ function randomIntFromInterval(min, max) { // min and max included
 }
 
 /**
- * This file is used to test multiple archaeologists locally
+ * This file is used to run multiple archaeologists locally
  * Set numOfArchsToGenerate for how many archaeologists to generate
  */
 
-const numOfArchsToGenerate = 1
+const numOfArchsToGenerate = 2
 const startingTcpPort = randomIntFromInterval(10000, 15000)
 const startingWsPort = randomIntFromInterval(15001, 20000)
-const bootstrapEncryptionWallet = new ethers.Wallet(process.env.ENCRYPTION_PRIVATE_KEY!);
 
 let archInitNodePromises: Promise<void | Libp2p>[] = [];
-
-/**
- * Setup and start Bootstrap Node
- */
-
-const peerId = await loadPeerIdFromFile();
-
-const { listenAddresses } = await randomArchVals(
-  startingTcpPort, startingWsPort
-)
-
-const bootstrap = new Archaeologist({
-  name: "bootstrap",
-  peerId,
-  listenAddresses,
-  isBootstrap: true
-})
-
-await bootstrap.initNode({
-  config: { encryptionPublicKey: bootstrapEncryptionWallet.publicKey },
-  web3Interface: await getWeb3Interface(true),
-});
-
-/**
- * Setup and start non-bootstrap nodes
- */
-
-const bootstrapList = getMultiAddresses(bootstrap.node)
 
 // Nodes will start with this delay between them
 const delayIncrement = 2000;
@@ -69,7 +36,6 @@ for (let i = 1; i <= numOfArchsToGenerate; i++) {
           nodeName: `arch${i}`,
           peerId,
           listenAddresses,
-          bootstrapList,
           isTest: true,
         })
       )
