b64b27226817b7ec075d2474bb9745d337c60331
Breads <breads.@Breadss-MacBook-Pro.local>
2022-10-04 16:47:13 -0600

    add lf

--- service/src/utils/error-codes.ts
+++ service/src/utils/error-codes.ts
@@ -2,14 +2,14 @@
 // and use for consistency
 
 export interface StreamCommsError {
-    code: number,
-    message: string,
+  code: number,
+  message: string,
 }
 
 export enum SarcophagusValidationError {
-   UNKNOWN_ERROR = 1,
-   MAX_REWRAP_INTERVAL_TOO_LARGE = 2,
-   INVALID_ARWEAVE_SHARD = 3,
-   DIGGING_FEE_TOO_LOW = 4,
-   INVALID_TIMESTAMP = 5
+  UNKNOWN_ERROR = 1,
+  MAX_REWRAP_INTERVAL_TOO_LARGE = 2,
+  INVALID_ARWEAVE_SHARD = 3,
+  DIGGING_FEE_TOO_LOW = 4,
+  INVALID_TIMESTAMP = 5
 }
