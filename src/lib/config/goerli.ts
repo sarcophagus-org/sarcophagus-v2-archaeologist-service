e7bb9533a7a0096c69e8787866985381e2744fc5
Seth Hrbek <43755993+sethhrbek@users.noreply.github.com>
2022-12-02 08:59:48 -0500

    Revert "update diamond address"

--- src/lib/config/goerli.ts
+++ src/lib/config/goerli.ts
@@ -5,7 +5,7 @@ export const goerliNetworkConfig: NetworkConfig = {
   networkName: "Goerli Testnet",
   networkShortName: "Goerli",
   sarcoTokenAddress: "0x4633b43990b41B57b3678c6F3Ac35bA75C3D8436",
-  diamondDeployAddress: "0xB7c129B3A4717A50b6693b8D831851D41BD18a7c",
+  diamondDeployAddress: "0x814De2Db5D12E7e10B79D128Fca70Baba53d8394",
   arweave: {
     host: "arweave.net",
     port: 443,
