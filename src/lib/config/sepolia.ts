b1793320b8a0c2e9bedfe1eda18c28c453cfb181
Kellar <kellar.base09@gmail.com>
2022-12-21 12:54:09 +0000

    Add sepolia contract addresses

--- src/lib/config/sepolia.ts
+++ src/lib/config/sepolia.ts
@@ -4,8 +4,8 @@ export const sepoliaNetworkConfig: NetworkConfig = {
   chainId: 11155111,
   networkName: "Sepolia Testnet",
   networkShortName: "Sepolia",
-  sarcoTokenAddress: "",
-  diamondDeployAddress: "",
+  sarcoTokenAddress: "0x663D82908Bd9f2735A5E5F7ceCA1e00F4ef39363",
+  diamondDeployAddress: "0x21164Ec3AF79752DB98b908ff740731E80486027",
   arweave: {
     host: "arweave.net",
     port: 443,
