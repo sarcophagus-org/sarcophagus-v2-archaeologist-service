320aec44e848a9e8c82fd5bb0bcbd933419c93e9
sethhrbek <seth.hrbek@gmail.com>
2023-03-28 09:53:28 -0400

    add rewrap logging

--- src/utils/scheduler.ts
+++ src/utils/scheduler.ts
@@ -4,8 +4,9 @@ import { publishPrivateKey } from "./blockchain/publish-private-key";
 import { inMemoryStore } from "./onchain-data";
 
 const scheduledPublishPrivateKey: Record<string, scheduler.Job | undefined> = {};
+const sarcoIdToResurrectionTime: Record<string, number> = {};
 
-export function schedulePublishPrivateKey(sarcoId: string, date: Date) {
+export function schedulePublishPrivateKey(sarcoId: string, resurrectionTime: Date, exactResurrectionTime: number) {
   // If sarcophagus is being unwrapped, dont schedule job
   const sarcoIndex = inMemoryStore.sarcoIdsInProcessOfHavingPrivateKeyPublished.findIndex(
     id => id === sarcoId
@@ -16,12 +17,20 @@ export function schedulePublishPrivateKey(sarcoId: string, date: Date) {
   }
 
   if (!scheduledPublishPrivateKey[sarcoId]) {
-    archLogger.notice(`Scheduling unwrap for ${sarcoId} at: ${date.getTime() / 1000} (${date.toString()})`);
+    archLogger.notice(`Scheduling unwrap for ${sarcoId} at: ${resurrectionTime.getTime() / 1000} (${resurrectionTime.toString()})`);
+  } else {
+    // If time is different than one in memory, a rewrap has occurred
+    if (sarcoIdToResurrectionTime[sarcoId] !== exactResurrectionTime) {
+      archLogger.notice(`Scheduling rewrap for ${sarcoId} at: ${resurrectionTime.getTime() / 1000} (${resurrectionTime.toString()})`);
+    }
   }
+
+  // Stored just for purposes of logging
+  sarcoIdToResurrectionTime[sarcoId] = exactResurrectionTime;
 
   // Cancel existing schedules, so no duplicate jobs will be created.
   scheduledPublishPrivateKey[sarcoId]?.cancel();
-  scheduledPublishPrivateKey[sarcoId] = scheduler.scheduleJob(date, async () => {
+  scheduledPublishPrivateKey[sarcoId] = scheduler.scheduleJob(resurrectionTime, async () => {
     await publishPrivateKey(sarcoId);
   });
 }
