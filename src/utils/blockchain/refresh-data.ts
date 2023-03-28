b0ee9610438bc12b6ff387d0ac2362361bebf74a
sethhrbek <seth.hrbek@gmail.com>
2023-03-28 11:11:38 -0400

    change mapping to a for loop

--- src/utils/blockchain/refresh-data.ts
+++ src/utils/blockchain/refresh-data.ts
@@ -31,71 +31,72 @@ export async function fetchSarcophagiAndSchedulePublish(): Promise<SarcophagusDa
 
   const currentBlockTimestampSec =  await getBlockTimestamp();
 
-  sarcoIds
-    .filter(id => !inMemoryStore.deadSarcophagusIds.includes(id))
-    .map(async sarcoId => {
-      try {
-        const sarcophagus = await web3Interface.viewStateFacet.getSarcophagus(sarcoId);
+  const activeSarcoIds = sarcoIds.filter(id => !inMemoryStore.deadSarcophagusIds.includes(id));
 
-        // If sarcophagus is buried, cleaned or compromised, don't scheduled an unwrap
-        if (isSarcoInactive(sarcophagus)) {
-          inMemoryStore.deadSarcophagusIds.push(sarcoId);
-          return;
-        }
+  archLogger.debug('starting resync');
+  for (const sarcoId of activeSarcoIds) {
+    try {
+      const sarcophagus = await web3Interface.viewStateFacet.getSarcophagus(sarcoId);
 
-        // If the current time is past the grace period, don't schedule an unwrap
-        const tooLateToUnwrap = (currentBlockTimestampSec > endOfGracePeriod(sarcophagus, inMemoryStore.gracePeriod!));
-
-        if (tooLateToUnwrap) {
-          archLogger.debug(`Too late to unwrap: ${sarcoId} with resurrection time: ${sarcophagus.resurrectionTime.toNumber()} -- current time is ${Date.now() / 1000}`);
-          inMemoryStore.deadSarcophagusIds.push(sarcoId);
-          return;
-        }
-
-        const archaeologist = await web3Interface.viewStateFacet.getSarcophagusArchaeologist(
-          sarcoId,
-          web3Interface.ethWallet.address
-        );
-
-        if (archStillNeedsToPublishPrivateKey(archaeologist)) {
-          // Account for out of sync system clocks
-          // Scheduler will use the system clock which may not be in sync with block.timestamp
-          const systemClockDifferenceSecs = Math.round((Date.now() / 1000) - currentBlockTimestampSec);
-          archLogger.debug(`currentBlockTimestampSec is ${currentBlockTimestampSec}`);
-          archLogger.debug(`systemClockDifference is ${systemClockDifferenceSecs}`);
-
-          // NOTE: If we are past the resurrection time (but still in the grace period)
-          // Then schedule the unwrap for 5 seconds from now. Otherwise schedule for resurrection time
-          // (plus 15 seconds to allow block.timestamp to advance past resurrection time).
-          archLogger.debug(`resurrectionTime raw: ${sarcophagus.resurrectionTime.toNumber()}`);
-
-          const isResurrectionTimeInPast = currentBlockTimestampSec > sarcophagus.resurrectionTime.toNumber();
-          let scheduledResurrectionTime;
-
-          if (isResurrectionTimeInPast) {
-            archLogger.debug(`resurrection time is in the past, scheduling for 5 seconds from now`);
-            scheduledResurrectionTime = new Date(Date.now() + 5000);
-          } else {
-            // schedule resurrection time, taking into account system clock differential + buffer
-            scheduledResurrectionTime = new Date(((sarcophagus.resurrectionTime.toNumber() + systemClockDifferenceSecs) * 1000) + 15_000);
-          }
-
-          archLogger.debug(`resurrection time with buffer: ${scheduledResurrectionTime}`);
-
-          schedulePublishPrivateKey(sarcoId, scheduledResurrectionTime, sarcophagus.resurrectionTime.toNumber());
-
-          sarcophagi.push({
-            id: sarcoId,
-            resurrectionTime: scheduledResurrectionTime,
-          });
-        } else {
-          // Save inactive ones in memory to save RPC calls on next re-sync
-          inMemoryStore.deadSarcophagusIds.push(sarcoId);
-        }
-      } catch (e) {
-        handleRpcError(e);
+      // If sarcophagus is buried, cleaned or compromised, don't scheduled an unwrap
+      if (isSarcoInactive(sarcophagus)) {
+        inMemoryStore.deadSarcophagusIds.push(sarcoId);
+        continue;
       }
-    });
+
+      // If the current time is past the grace period, don't schedule an unwrap
+      const tooLateToUnwrap = (currentBlockTimestampSec > endOfGracePeriod(sarcophagus, inMemoryStore.gracePeriod!));
+
+      if (tooLateToUnwrap) {
+        archLogger.debug(`Too late to unwrap: ${sarcoId} with resurrection time: ${sarcophagus.resurrectionTime.toNumber()} -- current time is ${Date.now() / 1000}`);
+        inMemoryStore.deadSarcophagusIds.push(sarcoId);
+        continue;
+      }
+
+      const archaeologist = await web3Interface.viewStateFacet.getSarcophagusArchaeologist(
+        sarcoId,
+        web3Interface.ethWallet.address
+      );
+
+      if (archStillNeedsToPublishPrivateKey(archaeologist)) {
+        // Account for out of sync system clocks
+        // Scheduler will use the system clock which may not be in sync with block.timestamp
+        const systemClockDifferenceSecs = Math.round((Date.now() / 1000) - currentBlockTimestampSec);
+        archLogger.debug(`currentBlockTimestampSec is ${currentBlockTimestampSec}`);
+        archLogger.debug(`systemClockDifference is ${systemClockDifferenceSecs}`);
+
+        // NOTE: If we are past the resurrection time (but still in the grace period)
+        // Then schedule the unwrap for 5 seconds from now. Otherwise schedule for resurrection time
+        // (plus 15 seconds to allow block.timestamp to advance past resurrection time).
+        archLogger.debug(`resurrectionTime raw: ${sarcophagus.resurrectionTime.toNumber()}`);
+
+        const isResurrectionTimeInPast = currentBlockTimestampSec > sarcophagus.resurrectionTime.toNumber();
+        let scheduledResurrectionTime;
+
+        if (isResurrectionTimeInPast) {
+          archLogger.debug(`resurrection time is in the past, scheduling for 5 seconds from now`);
+          scheduledResurrectionTime = new Date(Date.now() + 5000);
+        } else {
+          // schedule resurrection time, taking into account system clock differential + buffer
+          scheduledResurrectionTime = new Date(((sarcophagus.resurrectionTime.toNumber() + systemClockDifferenceSecs) * 1000) + 15_000);
+        }
+
+        archLogger.debug(`resurrection time with buffer: ${scheduledResurrectionTime}`);
+
+        schedulePublishPrivateKey(sarcoId, scheduledResurrectionTime, sarcophagus.resurrectionTime.toNumber());
+
+        sarcophagi.push({
+          id: sarcoId,
+          resurrectionTime: scheduledResurrectionTime,
+        });
+      } else {
+        // Save inactive ones in memory to save RPC calls on next re-sync
+        inMemoryStore.deadSarcophagusIds.push(sarcoId);
+      }
+    } catch (e) {
+      handleRpcError(e);
+    }
+  }
 
   return sarcophagi;
 }
