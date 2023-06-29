b319822d5a65b2a5513525dfce7b91f8b2d39309
knoll3 <kyle.noll@decentlabs.io>
2023-06-29 15:13:36 -0500

    Update mainnet network config

--- src/lib/config/mainnet.ts
+++ src/lib/config/mainnet.ts
@@ -4,6 +4,9 @@ export const mainnetNetworkConfig: NetworkConfig = {
   chainId: 1,
   networkName: "Mainnet",
   networkShortName: "Mainnet",
-  sarcoTokenAddress: "",
+  sarcoTokenAddress: "0x7697b462a7c4ff5f8b55bdbc2f4076c2af9cf51a",
+  // TODO: Add mainnet diamond address
   diamondDeployAddress: "",
+  // TODO: Add mainnet subgraph url
+  subgraphUrl: "",
 };
