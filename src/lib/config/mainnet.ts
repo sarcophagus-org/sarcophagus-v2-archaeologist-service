2ac1db5d8f276c2f5917a7b9b21e7136ab283ab5
Seth Hrbek <seth.hrbek@gmail.com>
2023-06-30 17:22:13 -0400

    update mainnet network config

--- src/lib/config/mainnet.ts
+++ src/lib/config/mainnet.ts
@@ -4,6 +4,6 @@ export const mainnetNetworkConfig: NetworkConfig = {
   chainId: 1,
   networkName: "Mainnet",
   networkShortName: "Mainnet",
-  sarcoTokenAddress: "",
-  diamondDeployAddress: "",
+  sarcoTokenAddress: "0x7697b462a7c4ff5f8b55bdbc2f4076c2af9cf51a",
+  diamondDeployAddress: "0x0Ec977D1863Eb919a2Ecd65d17Cc3f2CFdaCe3Ab",
 };
