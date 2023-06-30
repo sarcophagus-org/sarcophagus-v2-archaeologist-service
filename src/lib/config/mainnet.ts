39b39060c99919d405b0edb350cd7c386b09eeba
Seth Hrbek <seth.hrbek@gmail.com>
2023-06-30 17:23:22 -0400

    add sg mainnet url

--- src/lib/config/mainnet.ts
+++ src/lib/config/mainnet.ts
@@ -6,4 +6,5 @@ export const mainnetNetworkConfig: NetworkConfig = {
   networkShortName: "Mainnet",
   sarcoTokenAddress: "0x7697b462a7c4ff5f8b55bdbc2f4076c2af9cf51a",
   diamondDeployAddress: "0x0Ec977D1863Eb919a2Ecd65d17Cc3f2CFdaCe3Ab",
+  subgraphUrl: "https://api.studio.thegraph.com/query/49076/sarcophagus-v2/v0.0.1",
 };
