57184513dc377745078ea64ae75eb4f58308ce84
Kellar <kellar.base09@gmail.com>
2023-09-22 17:51:27 +0100

    cleanup wssListenAddress

--- src/utils/listen-addresses.ts
+++ src/utils/listen-addresses.ts
@@ -13,21 +13,7 @@ export const genListenAddresses = (
 
 export const wssListenAddress = (chainId: number): string[] => {
   archLogger.debug("using websockets");
-
-  // To support running the archaeologist service on multiple networks simultaneously,
-  // we need to listen on a different port for each network.
-  // We use the chainId to determine which port to use. These ports are hardcoded in the
-  // docker-compose.yml file of the quickstart-archaeologist repo.
-  // TODO: for cleaner looking code, we could import these ports from the sdk.
-  const chainIdTolistenPort = new Map<number, string>([
-    [1, "9000"], // mainnet
-    [11155111, "9001"], // sepolia
-    [5, "9002"], // goerli
-    [84531, "9003"], // baseGoerli
-    [80001, "9004"], // Polygon Mumbai
-  ]);
-
-  return [`/ip4/127.0.0.1/tcp/${chainIdTolistenPort[chainId]}/wss`];
+  return [`/ip4/127.0.0.1/tcp/9000/wss`];
 };
 
 export const ssListenAddresses = (
