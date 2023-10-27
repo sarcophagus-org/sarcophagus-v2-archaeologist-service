2f21534c20705c6821c85ae142f1bc3bb9f1ddaa
Seth Hrbek <seth.hrbek@gmail.com>
2023-10-27 12:21:27 -0400

    update claim config

--- src/cli/commands/reward.ts
+++ src/cli/commands/reward.ts
@@ -3,17 +3,17 @@ import { getWeb3Interface } from "../../scripts/web3-interface";
 import { exit } from "process";
 import { RPC_EXCEPTION, SUCCESS } from "../../utils/exit-codes";
 import { NetworkContext } from "../../network-config";
-import { logValidationErrorAndExit } from "../../cli/utils";
 import { archLogger } from "../../logger/chalk-theme";
 import { handleRpcError } from "../../utils/rpc-error-handler";
 import { retryFn } from "../../utils/blockchain/helpers";
+import { claimOptionDefinitions } from "../config/claim-args";
 
 export class Reward implements Command {
   name = "claim";
   aliases = ["c"];
   description = "Claim all rewards you have earned";
   shouldBeRegistered: boolean;
-  args = [];
+  args = claimOptionDefinitions;
   networkContext: NetworkContext;
 
   constructor() {
@@ -33,15 +33,6 @@ export class Reward implements Command {
     } catch (error) {
       await handleRpcError(error, this.networkContext);
       exit(RPC_EXCEPTION);
-    }
-  }
-
-  validateArgs(options: CommandOptions) {
-    const multipleChains = process.env.CHAIN_IDS!.split(",").length > 1;
-    if (multipleChains && !options.network) {
-      logValidationErrorAndExit(
-        "Missing network option. Use --network to specify a network to run this command on."
-      );
     }
   }
 
